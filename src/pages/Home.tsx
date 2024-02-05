c3a88420ce0d221e443443b2a077bfe75066f877
Rediet Tekola <RDT7992@gmail.com>
2024-02-06 01:12:26 +0300

    add style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,10 +3,66 @@ import { useEffect } from "react";
 import { deleteSong, getSongs } from "../redux/songSlice";
 import { Link } from "react-router-dom";
 import { IState } from "../types";
+import styled from "@emotion/styled";
+
+const Container = styled.div`
+  min-width: 760px;
+  width: 100%;
+`;
+
+const TitleContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  gap: 1rem;
+  > h1 {
+    margin: 0;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  padding: 8px;
+  background-color: #22c55e;
+  color: white;
+  border-radius: 4px;
+
+  :hover {
+    color: #e2e8f0;
+  }
+`;
+
+const Table = styled.table`
+  margin: 2rem 0;
+  text-align: center;
+  /* border-collapse: collapse; */
+
+  /* border: 1px solid #d946ef; */
+  /* border-radius: 4px; */
+
+  tr {
+    margin: 8px 0;
+    background-color: #f1f5f9;
+  }
+
+  td,
+  th {
+    width: 100px;
+    padding: 0.5rem 1rem;
+    /* margin: 1rem; */
+    text-align: center;
+    /* border: 1px solid #d946ef; */
+    border-radius: 8px;
+  }
+`;
+
+const Button = styled.button`
+  background-color: red;
+  color: white;
+`;
 
 const Home = () => {
   const { isLoading, songs } = useSelector((state: IState) => state.songs);
-  console.log("🚀 ~ Home ~ songs:", songs);
 
   const dispatch = useDispatch();
 
@@ -15,15 +71,15 @@ const Home = () => {
   }, [dispatch]);
 
   return (
-    <>
-      <div>
+    <Container>
+      <TitleContainer>
         <h1>Manage songs</h1>
-        <Link to="/songs/new">Add New song</Link>
-      </div>
+        <StyledLink to="/songs/new">Add New song</StyledLink>
+      </TitleContainer>
       {isLoading ? (
         <h5>Loading</h5>
       ) : (
-        <table>
+        <Table>
           <thead>
             <tr>
               <th>Title</th>
@@ -45,16 +101,16 @@ const Home = () => {
                   <Link to={`/songs/${song._id}`}>Edit</Link>
                 </td>
                 <td>
-                  <button onClick={() => dispatch(deleteSong(song._id))}>
+                  <Button onClick={() => dispatch(deleteSong(song._id))}>
                     delete
-                  </button>
+                  </Button>
                 </td>
               </tr>
             ))}
           </tbody>
-        </table>
+        </Table>
       )}
-    </>
+    </Container>
   );
 };
 
