ab000d381b7b348c9bdbfeac6ac668d85f4107fb
Rediet Tekola <43878585+Rdttkab@users.noreply.github.com>
2024-02-09 13:14:49 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    checker({ typescript: false }),
     react({
       jsxImportSource: "@emotion/react",
       babel: {
